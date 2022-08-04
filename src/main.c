ca343818e25ad59c989a36cb67fb31181ea5206d
sergej <for-my-reg@yandex.ru>
2022-08-04 21:21:54 +0300

    add uasrt anf timer isr

--- src/main.c
+++ src/main.c
@@ -1,25 +1,41 @@
+
 #include "main.h"
 
+#include <util/delay.h>
+
+uint8_t message[] = "test message\n";
 ISR(TIMER1_COMPA_vect) {
+	if(TESTBIT(PORTD, PIND0)) {
+		CLEARBIT(PORTD, PIND0);
+	} else {
+		SETBIT(PORTD, PIND0);
+	}
 }
 
 ISR(USART_TXC_vect) {
-	static uint8_t old_symbol;
+	UDR='H';
 }
 
 
 
 int main(void) {
-
-  	OCR1A = _F_CPU / TIMERFRQ - 1;
+  	OCR1A = F_CPU / TIMERFRQ - 1;
 	TCCR1B = (1 << CS10) | (1 << WGM12);
  	TIMSK = TIMSK | (1 << OCIE1A);
 
 	UBRRH = (uint8_t) (UART_UBRR >> 8);
 	UBRRL = (uint8_t) UART_UBRR;
 	/* Enable receiver and transmitter */
-	UCSRB = (1<<RXEN)|(1<<TXEN);
+	UCSRB = (1 << TXEN) | (1<<TXCIE);
 	/* Set frame format: 8data, 2stop bit */
-	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0);
-	while(1);
+	UCSRC = (1 << URSEL) | (1 << USBS) | (1 << UCSZ0) | (1 << UCSZ1);
+	DDRD = 0xFF; //PORTD as output
+	SETBIT(PORTD, PIND0);
+
+	sei();
+	while ( !( UCSRA & (1<<UDRE)));
+	UDR='H';	
+	while(1){
+	};
+	return 0;
 }
--- src/main.h
+++ src/main.h
@@ -5,10 +5,10 @@
 #include <avr/interrupt.h>
 #include "avr/eeprom.h"
 
-#define _F_CPU				11059200UL
-#define	TIMERFRQ			1000UL
-#define BAUD                9600
-#define UART_UBRR           _F_CPU / 16 / BAUD-1
+#define F_CPU				            8000000UL//11059200UL
+#define	TIMERFRQ			            1000UL
+#define BAUD                            9600
+#define UART_UBRR                       (F_CPU / 16 / BAUD) - 1
 
 /* 
  * Set BIT in ADDRESS 
